4faac3682a9ae3b9942696874786d7fb19ff0c6a
elperez365 <ceti.vasco@gmail.com>
2023-09-03 15:37:10 +0200

    kmForFun

--- src/KmForFun.java
+++ src/KmForFun.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class KmForFun {
-    
+    static ArrayList<Integer> tempi = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("inserisci tempo" + String.valueOf(i) + " in minuti");
+            int tempo = scanner.nextInt();
+            KmForFun.tempi.add(tempo);
+
+        }
+        scanner.close();
+        int max = 0;
+        int min;
+
+        for (int tempo : KmForFun.tempi) {
+            max = tempo > max ? tempo : max;
+        }
+
+        min = max;
+
+        for (int tempo : KmForFun.tempi) {
+            min = tempo < min ? tempo : min;
+        }
+
+        System.out.println("il tempo migliore è stato : " + min + " minuti");
+        System.out.println("il tempo peggiore è stato : " + max + " minuti");
+
+    }
 }
